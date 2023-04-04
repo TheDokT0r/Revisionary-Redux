29f2fbfc579aaa974847aff19b486a1e40cab06a
TheDokTor <orkv1234@gmail.com>
2023-04-04 11:27:40 +0300

    better browsing screen

--- src/pages/Revitions/Browse/Browse.jsx
+++ src/pages/Revitions/Browse/Browse.jsx
@@ -1,23 +1,40 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import Navbar from '../../../components/Navbar/Navbar';
 import fetchRevitions from '../../../api/RevitionsMannagement/fetchRevitions';
 import RevitionData from '../../../components/RevitionData';
+import LoadingScreen from '../../../components/LoadingScreen';
 
 export default function Browse() {
   const [revitions, setRevitions] = useState([]);
+  const [loadingSearch, setLoadingSearch] = useState(false);
+
+  const searchKeywords = useRef('');
+
+
+  const fetchRevitionsFromDatabase = async () => {
+    setLoadingSearch(true);
+
+    const revitions = await fetchRevitions(searchKeywords.current);
+    setRevitions(revitions);
+
+    setLoadingSearch(false);
+  }
+
+
+  const searchHandeler = async (e) => {
+    e.preventDefault();
+
+    await fetchRevitionsFromDatabase();
+  }
 
   useEffect(() => {
-    const fetch = async () => {
-      const revitions = await fetchRevitions();
-      setRevitions(revitions);
-    }
-    fetch();
+    fetchRevitionsFromDatabase();
   }, []);
-
-  console.log(revitions);
 
 
   const displayRevitions = () => {
+    if (revitions.length === 0) return (<p>No Revitions Found</p>);
+
     return revitions.map(revition => {
       return <RevitionData revition={revition} />
     })
@@ -29,7 +46,17 @@ export default function Browse() {
       <Navbar />
       <div>
         <h1>Browse</h1>
-        {displayRevitions()}
+
+        <form>
+          <input
+            type="text"
+            placeholder="Search"
+            onChange={(e) => {
+              searchKeywords.current = e.target.value
+            }} />
+          <button onClick={searchHandeler}>Search</button>
+        </form>
+        {loadingSearch ? <LoadingScreen text={'Fetching revitions'}/> : displayRevitions()}
       </div>
     </>
   )
